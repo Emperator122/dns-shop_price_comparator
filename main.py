ff35404a4383ca488a8b05b5b0c17bcfadc785fb
Shigin D <empirefp@mail.ru>
2022-06-03 01:03:28 +0300

    add prints

--- main.py
+++ main.py
@@ -2,10 +2,10 @@ from network.api.auth import AuthRepository
 from network.api.orders import OrdersRepository
 from network.api.products import ProductsRepository
 from network.pages_renderer import PagesRenderer
-from selenium import webdriver
 from models.api_produtct import APIOrdersGroup
 import csv
 
+# input data
 LOGIN = '197012008@mail.ru'
 PASSWORD = 'a12181218'
 CSV_HEADER = ['url', 'name', 'status', 'delta']
@@ -22,16 +22,20 @@ orders_repository = OrdersRepository(user_agent, cookies)
 products_repository = ProductsRepository(user_agent, cookies)
 
 # login
+print('Logining in...')
 auth_repository.login(LOGIN, PASSWORD)
 
 # get user's orders
+print('Loading user\'s orders...')
 orders_groups = orders_repository.get_all_my_orders()
 products = APIOrdersGroup.get_products_from_paginated_groups(orders_groups)
 
 # get actual prices
+print('Loading actual prices...')
 actual_products_microdata = products_repository.get_microdata_from_products_groups(products)
 
 # save to csv
+print('Saving...')
 with open('delta.csv', 'w', newline='') as f:
     writer = csv.writer(f, delimiter=';')
     # insert header
@@ -49,3 +53,4 @@ with open('delta.csv', 'w', newline='') as f:
             else -1,
         ]
         writer.writerow(row)
+print('Saved!')
